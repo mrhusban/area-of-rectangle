c647720bc343604c7fa93088816d5525ea81a580
NTC <NTC@HUSBAN>
2021-08-28 18:47:33 +0300

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int width , length, area;
+	cout << " enter rectangle width : \n";
+	cin >> width;
+
+	cout << " enter rectangle length : \n";
+	cin >> length;
+	
+	area = width * length;
+	cout << " a area recangle is : \t " << area;
+	return 0;
+
+
+}
